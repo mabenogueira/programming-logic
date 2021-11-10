71a184592b991eb2c094694cda848477c901e0b0
Maria Beatriz <mrbtrznogueira@gmail.com>
2021-11-10 20:50:44 -0300

    1015

---
+++ C#/Beginner/URI/1015 - Distancia entre dois pontos.cs
@@ -0,0 +1,20 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    string[] a = Console.ReadLine().Split(' ');
+    string[] b = Console.ReadLine().Split(' ');
+
+
+    double x1 = double.Parse(a[0]);
+    double y1 = double.Parse(a[1]);
+
+    double x2 = double.Parse(b[0]);
+    double y2 = double.Parse(b[1]);
+
+    double distancia = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
+    Console.WriteLine($"{distancia:0.0000}");
+
+  }
+}
