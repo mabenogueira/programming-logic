08218c6b185f0173aa20f92acf66695018a7e32d
Maria Beatriz <mrbtrznogueira@gmail.com>
2021-11-10 20:48:00 -0300

    Create  1001 - Extremamente basico.cs

---
+++ C#/Beginner/URI/1001 - Extremamente basico.cs
@@ -0,0 +1,14 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    int a, b, soma;
+
+    a = int.Parse(Console.ReadLine());
+    b = int.Parse(Console.ReadLine());
+
+    soma = a+b;
+    Console.WriteLine("X = " + soma);
+  }
+}
